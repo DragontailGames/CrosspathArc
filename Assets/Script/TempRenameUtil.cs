5073d020385f1105f7a8d6731e1451efbd5004dd
Pedro Armando <pedro.armando15@hotmail.com>
2021-04-05 00:32:00 -0300

    Adicionando Animação feminia

---
+++ Assets/Script/TempRenameUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TempRenameUtil : MonoBehaviour
+{
+    public string folder = "";
+    public string replace_name = "";
+    public string new_name = "";
+
+    [ContextMenu("Command")]
+    void RenameFiles()
+    {
+        foreach (var asset in AssetDatabase.FindAssets("", new[] { folder }))
+        {
+            var path = AssetDatabase.GUIDToAssetPath(asset);
+            var temp =  AssetDatabase.LoadAssetAtPath(path, typeof(Object));
+            var name = temp.name.Replace(replace_name, new_name);
+            AssetDatabase.RenameAsset(path, name);
+        }
+    }
+}
